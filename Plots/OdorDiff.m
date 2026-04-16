c81a0b189e5ba07e1abd891da06982874a86cd5b
Vinicius Ambrosi <42151837+vambrosi@users.noreply.github.com>
2026-04-16 03:12:46 -0700

    Add z-score difference plot

---
+++ Plots/OdorDiff.m
@@ -0,0 +1,156 @@
+%{
+Odor Difference
+written by VA on Apr/2026
+
+GOAL:
+    Plots the difference between the z-scores of dF/F for two odors
+
+DEPENDS:
+    - image_dF_* figures struct.
+
+%}
+
+%% USER INPUT
+
+% Save folder
+saveFolder = '.';
+
+% Odor numbers (order doesn't matter)
+firstOdorId = 3;
+secondOdorId = 20;
+
+% hit, miss, false, or na?
+outcome = "na";
+
+% Range for the z-scores
+zScoreMax = 5;
+
+% Path to .mat files (could be more than one)
+pathMatFiles = ["/Users/vinicius/TempData/20260324/m357/e1/processed/matlab/2026-04-11/20260324_m357_e1_00001_corrected_w_ted_warped_preProcessing.mat"];
+
+% Program numbers
+programNumbers = [1, 2];
+
+% Define the colors
+max_df_color = [103 0 31] / 255;
+min_df_color = [5 48 97] / 255;
+
+%% Creates Diverging Colormap
+
+% Creates 512 color steps between the two colors (512 is arbitrary)
+% The number just need to be high enough for the gradient to be smooth
+colorpoints = linspace(0.0, 1.0, 512);
+
+% This function is from the following website:
+% https://www.kennethmoreland.com/color-maps/
+divergingGradient = divergingMap(colorpoints, min_df_color, max_df_color);
+
+%% Plot the figures
+
+function isIt = isRightFig(s, odorID, programNumber)
+    isIt = s.odor == odorID && s.programNumber == programNumber;
+end
+
+% Create figure
+fig = figure;
+
+nRows = length(pathMatFiles) * length(programNumbers);
+
+tl = tiledlayout(fig, nRows, 3);
+title(tl, "Z-score Change");
+
+iRow = 1;
+
+% Plot ranges
+bidirectionalRange = [-zScoreMax zScoreMax];
+onlyPositiveRange = [0 zScoreMax];
+
+% One new row for each combination of experiment and program
+for path = pathMatFiles
+    load(path, 'figures', 'firstAcqName');
+
+    if iRow == 1
+        figName = split(firstAcqName, '_');
+        figName = join(figName(1:3), '_');
+        figName = figName{1};
+        startFigName = true;
+
+    else
+        addToFigName = split(firstAcqName, '_');
+        figName = sprintf("%s_%s", figName, addToFigName{3});
+
+    end
+
+
+    for programNumber = programNumbers
+        % First odor plot
+        ax = nexttile;
+        findFun = arrayfun( ...
+            @(s) isRightFig(s, firstOdorId, programNumber), figures);
+        firstOdorData = figures(find(findFun, 1));
+
+        imshow(firstOdorData.(outcome).image, bidirectionalRange);
+
+        clim(ax, bidirectionalRange);
+        colormap(ax, divergingGradient);
+
+        if iRow == 1
+            title(sprintf("Odor %d", firstOdorId));
+
+        elseif iRow == nRows
+            colorbar(ax, 'southoutside');
+
+        end
+
+        % Second odor plot
+        ax = nexttile;
+        findFun = arrayfun( ...
+            @(s) isRightFig(s, secondOdorId, programNumber), figures);
+        secondOdorData = figures(find(findFun, 1));
+
+        imshow(secondOdorData.(outcome).image, bidirectionalRange);
+
+        clim(ax, bidirectionalRange);
+        colormap(ax, divergingGradient);
+
+        if iRow == 1
+            title(sprintf("Odor %d", secondOdorId));
+
+        elseif iRow == nRows
+            colorbar(ax, 'southoutside');
+
+        end
+
+        % Difference between images
+        ax = nexttile;
+
+        imshow( abs(secondOdorData.(outcome).image ...
+                  - firstOdorData.(outcome).image), onlyPositiveRange);
+
+        clim(ax, onlyPositiveRange);
+        colormap(ax, flipud(gray));
+
+        if iRow == 1
+            title("Absolute Difference")
+
+        elseif iRow == nRows
+            colorbar(ax, 'southoutside');
+
+        end
+
+        iRow = iRow + 1;
+    end
+end
+
+figName = strcat(figName, '_OdorDiff');
+fig.Name = figName;
+
+tl.TileSpacing = "tight";
+tl.Padding = "compact";
+
+
+%% Save figure
+
+figPath = fullfile(saveFolder, sprintf("%s.svg", figName));
+fig.Theme = 'light';
+saveas(fig, figPath);
